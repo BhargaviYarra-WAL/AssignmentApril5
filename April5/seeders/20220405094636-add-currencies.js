e9465748ae1ddca9dbe2c1383e1d2f9894707cf7
BhargaviYarra-WAL <bhargavi.y@westagilelabs.com>
2022-04-05 21:35:24 +0530

    adding

---
+++ April5/seeders/20220405094636-add-currencies.js
@@ -0,0 +1,46 @@
+"use strict";
+
+//const { query } = require("express");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Currencies",
+      [
+        {
+          name: "USD",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "EUR",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete("Currencies", {
+      [Op.or]: [{ name: "USD" }, { name: "EOR" }],
+    });
+  },
+};
